0a417f0515a0124b5aa69d3f7ad5891a9788ca14
evandro <evandro.azarias@alunos.unis.edu.br>
2020-02-21 22:10:05 -0300

    evandro0

---
+++ Projeto time/Projeto time/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Instancia de um objeto*/
+            Tme objTme = new Tme ();
+            objTme.nome = "Santos";
+            objTme.cidade = "Santos SP";
+            objTme.estado = "SP";
+            objTme.pais = "Brasil";
+            objTme.numeroDeTitulos = 89;
+            objTme.valorDoTimeEmDinheiro = 9000000;
+            objTme.numeroDeJogadores = 45 ;
+            objTme.comprarJogador(1);
+         
+          
+
+           
+
+            
+            
+            System.Console.WriteLine(@"Entre com a cidade do time "+ objTme.nome);
+            objTme.cidade = System.Console.ReadLine();
+
+
+            System.Console.WriteLine(@"Entre com o Pais da cidade " + objTme.cidade);
+            objTme.pais = System.Console.ReadLine();
+
+            System.Console.WriteLine(@"Entre com o estado do Pais " + objTme.pais);
+            objTme.estado = System.Console.ReadLine();
+
+            System.Console.WriteLine(@"Entre com o numero de titulos do time " + objTme.nome);
+            objTme.numeroDeTitulos = Convert.ToInt32( System.Console.ReadLine());
+
+            System.Console.WriteLine(@"Entre com o numero de jogadores do time " + objTme.nome);
+            objTme.numeroDeJogadores = Convert.ToInt32( System.Console.ReadLine());
+
+            System.Console.WriteLine(@"Entre com o valor do time " + objTme.nome);
+            objTme.valorDoTimeEmDinheiro = Convert.ToInt32(System.Console.ReadLine());
+
+            string statusDoObjeto;
+            statusDoObjeto = objTme.statusClass();
+            System.Console.WriteLine(statusDoObjeto);
+           
+            Console.ReadKey();
+            //int numJogador;
+            //System.Console.Readline(numJogador);
+            //objTme.comprarJogador(numJogador);
+        }
+    }
+}
---
+++ Projeto time/Projeto time/tme.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_time
+{
+    class Tme
+    {
+        /*Atributos( caracteristicas da classe)*/
+        public string nome;
+        public string pais;
+        public string estado;
+        public string cidade;
+        public int numeroDeJogadores = 0;
+        public int numeroDeTitulos = 0;
+        public double valorDoTimeEmDinheiro = 0;
+
+        /*metodos*/
+        public void comprarJogador(int numJongador)
+        {
+            this.numeroDeJogadores += numJongador;
+
+        }
+        public string statusClass()
+        {
+            string retorno;
+            retorno = "Nome::" + this.nome +
+                       "\nCidade:" + this.cidade +
+                       "\nEstado:" + this.estado +
+                       "\nPais:" + this.pais+
+                       " \nNumero de Jogadores:" + this.numeroDeJogadores +
+                       " \nNumero de Titulos:" + this.numeroDeTitulos +
+                       "\nValor do time: R$" + this.valorDoTimeEmDinheiro;
+
+            return retorno;
+
+        }
+
+    }
+}
